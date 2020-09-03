a34e2e1eb7b3e4a5b6be62315cc15b97ce784da4
lenkim <joenggyu0@gmail.com>
2020-09-03 17:33:29 +0900

    file commit

---
+++ src/main/java/org/slipp/DefaultConstant.java
@@ -0,0 +1,26 @@
+package org.slipp;
+
+public enum DefaultConstant {
+
+    DEFAULT_LIBRARY("spring-boot"),
+    TITLE("title"),
+    CONFIG_PACKAGE("configPackage"),
+    BASE_PACKAGE("basePackage"),
+    INTERFACE_ONLY("interfaceOnly"),
+    JAVA_8("java8"),
+    ASYNC("async"),
+    RESPONSE_WRAPPER("responseWrapper"),
+    USE_TAGS("useTags"),
+    IMPLICIT_HEADERS("implicitHeaders"),
+    HELLO("hello");
+
+    private final String value;
+
+    DefaultConstant(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
